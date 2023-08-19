657afa462e09ee83ffc6930c20d8ba6f3135239d
kayoGc <kayivciti@gmail.com>
2023-08-19 15:47:48 -0300

    adicionando hamburguer

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,9 @@
-function pesquisar() {
-    console.log("oi")
-}
+//  animação do hamburguer
+const hamburguer = document.querySelector(".hamburguer");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburguer.addEventListener("click", () => {
+    hamburguer.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
