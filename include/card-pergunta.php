42f70cf1bb24ebbe49e5bfc77ac5781b7a1e0932
kayoGc <kayivicti@gmail.com>
2023-08-25 21:51:54 -0300

    renovaçao

--- include/card-pergunta.php
+++ include/card-pergunta.php
@@ -4,11 +4,10 @@
     <header class="cardHeader">
         <!-- foto e nome usuario -->
         <div class="caixa-usuario">
-            <span class="material-symbols-outlined iconUsuario">person</span>
-            <span>Usuario</span>
+            <span class="txtUsuario">@Usuario</span>
         </div>
         <!-- Data -->
-        <date>12/12/1212</date>
+        <span class="date">12/12/1212</span>
     </header>
 
     <!-- Titulo -->
@@ -19,60 +18,8 @@
 
     <!-- footer do post -->
     <footer class="cardFooter">
-        <span class="material-symbols-outlined">favorite</span>
-        <span class="material-symbols-outlined">mode_comment</span>
-        <span class="material-symbols-outlined">bookmark</span>
-    </footer>
-</article>
-
-<article class="card">
-    <!-- Header do Post -->
-    <header class="cardHeader">
-        <!-- foto e nome usuario -->
-        <div>
-            <span class="material-symbols-outlined">person</span>
-            <span>Usuario</span>
-        </div>
-        <!-- Data -->
-        <date>12/12/1212</date>
-    </header>
-
-    <!-- Titulo -->
-    <h2 class="cardTitulo">Titulo Titulo Titulo Titulo Titulo</h2>
-
-    <!-- Texto do Post -->
-    <p class="cardText">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent consequat tortor quam, sed feugiat ipsum convallis nec. Suspendisse ligula massa, interdum.</p>
-
-    <!-- footer do post -->
-    <footer class="cardFooter">
-        <span class="material-symbols-outlined">favorite</span>
-        <span class="material-symbols-outlined">mode_comment</span>
-        <span class="material-symbols-outlined">bookmark</span>
-    </footer>
-</article>
-
-<article class="card">
-    <!-- Header do Post -->
-    <header class="cardHeader">
-        <!-- foto e nome usuario -->
-        <div>
-            <span class="material-symbols-outlined">person</span>
-            <span>Usuario</span>
-        </div>
-        <!-- Data -->
-        <date>12/12/1212</date>
-    </header>
-
-    <!-- Titulo -->
-    <h2 class="cardTitulo">Titulo Titulo Titulo Titulo Titulo</h2>
-
-    <!-- Texto do Post -->
-    <p class="cardText">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent consequat tortor quam, sed feugiat ipsum convallis nec. Suspendisse ligula massa, interdum ut diam facilisis, efficitur consectetur mi. Vestibulum facilisis sem arcu. Sed hendrerit, dolor quis accumsan molestie, augue ipsum elementum tortor.</p>
-
-    <!-- footer do post -->
-    <footer class="cardFooter">
-        <span class="material-symbols-outlined">favorite</span>
-        <span class="material-symbols-outlined">mode_comment</span>
-        <span class="material-symbols-outlined">bookmark</span>
+        <span class="material-symbols-outlined cardIcone">favorite</span>
+        <span class="material-symbols-outlined cardIcone">mode_comment</span>
+        <span class="material-symbols-outlined cardIcone">bookmark</span>
     </footer>
 </article>
